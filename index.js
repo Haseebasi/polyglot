a034988f0a7a6b89c6bcc106520aa974ac0de29a
Haseebasi <Haseebasi005@gmail.com>
2026-06-18 16:51:20 +0530

    initialised backend server

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const userInput = document.getElementById('user-input')
 // const langRadio = document.getquerySelectorAll('input[name="choose-language"]:checked')
 // const langValue = langRadio.value
 
-
-
-document.addEventListener('submit',function(e){
-    e.preventDefault();
-    console.log(e.target.id)
-    if(e.target.id === 'input-form'){
-        const french = document.getElementById("french")
+function inputFormHandler(){
+    const french = document.getElementById("french")
         const spanish = document.getElementById("spanish")
         const japanese = document.getElementById("japanese")
         let langValue = ""
@@ -23,15 +18,31 @@ document.addEventListener('submit',function(e){
         else if(japanese.checked){
             langValue = japanese.value
         }
+            
      if(userInput.value && langValue){
         console.log(userInput.value)
         console.log(langValue)
         inputForm.classList.add('hide')
         outputForm.classList.remove('hide')
-     }   
-    }
+     }else if(userInput.value){
+        alert('select a language')
+     }else{
+        alert('input some text')
+     }
+}
+function outputFormHandler(){
+        inputForm.classList.remove('hide')
+        outputForm.classList.add('hide')
+}
+
+
+document.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(e.target.id)
+    if(e.target.id === 'input-form'){
+            inputFormHandler()
+        }
     if(e.target.id === 'output-form'){
-        inputForm.classList.add('hide')
-        outputForm.classList.remove('hide')
+        outputFormHandler()
     }
 })
---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import OpenAi from "openai"
+import { Messages } from 'openai/resources/chat/completions.mjs'
+
+const app = express
+app.use(express.json)
+
+const openai = new OpenAI({
+    apiKey:"",
+    baseURL:"",
+    dangerouslyAllowBrowser:true
+})
+
+const messages = [
+    {
+        role:"system",
+        content:`you are translator app
+        remove all extra text`
+    }
+]
+
+
+
+const response = await openai.chat.completions.create({
+    model:"",
+    messages
+})
+
+
+
+// response.choices[0].message.content
